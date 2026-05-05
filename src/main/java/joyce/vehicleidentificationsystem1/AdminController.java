33f59f96de687b96e1d1f907cf87c8a3acd6f57f
TumoTranzi <koloitumo2@gmail.com>
2026-05-05 11:57:03 +0200

    Fix import statement formatting in AdminController

--- src/main/java/joyce/vehicleidentificationsystem1/AdminController.java
+++ src/main/java/joyce/vehicleidentificationsystem1/AdminController.java
@@ -3,7 +3,7 @@ package joyce.vehicleidentificationsystem1;
 import javafx.animation.FadeTransition;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
+import javafx.event.ActionEvent;  
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
